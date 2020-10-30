db71bb649c5c6952b36b146a9c6bd54de8fddaeb
Jivaka <jivakapyane93@gmail.com>
2020-10-30 13:02:39 -0700

    added alternate sources, timeblocks, app.js file and current date/time indicator

---
+++ Develop/app.js
@@ -0,0 +1,48 @@
+
+$(document).ready(function () {
+
+    console.log(parseInt(moment().format('H')))
+  
+    const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a')
+    $("#currentTime").text(currentTime)
+    $("span").attr("style", "width: 75px")
+    $("button").text("Submit")
+  
+    const times = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+  
+    times.forEach(time => {
+      const timeCheck = window.localStorage.getItem(time)
+  
+      const currentHour = moment().hour()
+  
+      console.log(currentHour)
+      console.log(time)
+  
+      if (currentHour > time) {
+        $(`#${time}`).addClass("bg-danger text-light")
+        $(`#${time}`).attr("disabled", true)
+      } else if (currentHour === time) {
+        $(`#${time}`).addClass("bg-secondary text-light")
+      } else {
+        $(`#${time}`).addClass("bg-success text-light")
+      }
+  
+      if (timeCheck === null) {
+        window.localStorage.setItem(time, "")
+      } else if (timeCheck.length > 0) {
+        $(`#${time}`).attr("value", window.localStorage.getItem(time))
+      }
+    })
+  
+  
+  
+  
+    $("form").on("submit", function (e) {
+      e.preventDefault()
+      const time = e.target.querySelector("input").getAttribute("id")
+      const text = e.target.querySelector("input").value
+  
+      window.localStorage.setItem(time, text)
+    })
+  })
+  
